49a8ab0bf28d8678e005ff964eef225a0e68b41d
kevmc <kevmckeon@gmail.com>
2021-03-10 16:42:49 +0000

    Added in studentNumber field to identify students. Added post method to create new student

--- src/main/java/com/school/application/controllers/StudentController.java
+++ src/main/java/com/school/application/controllers/StudentController.java
@@ -1,17 +1,23 @@
 package com.school.application.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.school.application.dtos.Student;
 import com.school.application.repositories.StudentRepository;
+
 
 @RestController
 public class StudentController {
@@ -24,9 +30,23 @@ public class StudentController {
 		return studentRepository.findAll();
 	}
 	
-	@GetMapping("students/{id}")
+/*	@GetMapping("students/{id}")
 	public Optional<Student> getAllStudents(@PathVariable Integer id) {
 		return studentRepository.findById(id);
+	}*/
+	
+	@GetMapping("students/{studentNumber}")
+	public Optional<Student> getStudentByStudentNumber(@PathVariable String studentNumber) {
+		return studentRepository.findByStudentNumber(studentNumber);
+	}
+	
+	@PostMapping("students/")
+	public ResponseEntity createWine(@RequestBody Student newStudent) {
+
+		Student savedStudent = studentRepository.save(newStudent);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(newStudent.getId()).toUri();
+		return ResponseEntity.created(location).build();
+
 	}
 
 	
--- src/main/java/com/school/application/dtos/Student.java
+++ src/main/java/com/school/application/dtos/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.school.application.enums.YearOfStudy;
@@ -14,8 +15,9 @@ import com.school.application.enums.YearOfStudy;
 public class Student {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  	// Required as the PK needs to be increased each time
 	private Integer 	id;
+	private String 		studentNumber;
 	private String 		forename;
 	private String 		surname;
 	private Date 		dateOfBirth;
@@ -28,55 +30,79 @@ public class Student {
 		super();
 	}
 
-	public Student(Integer id, String forename, String surname, Date dateOfBirth, YearOfStudy yearOfStudy) {
+		
+	public Student(Integer id, String studentNumber, String forename, String surname, Date dateOfBirth,
+			YearOfStudy yearOfStudy) {
+		super();
 		this.id = id;
+		this.studentNumber = studentNumber;
 		this.forename = forename;
 		this.surname = surname;
 		this.dateOfBirth = dateOfBirth;
 		this.yearOfStudy = yearOfStudy;
 	}
+
 
 	public Integer getId() {
 		return id;
 	}
 
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+
+	public String getStudentNumber() {
+		return studentNumber;
+	}
+
+
+	public void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+
 
 	public String getForename() {
 		return forename;
 	}
 
+
 	public void setForename(String forename) {
 		this.forename = forename;
 	}
+
 
 	public String getSurname() {
 		return surname;
 	}
 
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+
 
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
 
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+
 
 	public YearOfStudy getYearOfStudy() {
 		return yearOfStudy;
 	}
 
+
 	public void setYearOfStudy(YearOfStudy yearOfStudy) {
 		this.yearOfStudy = yearOfStudy;
 	}
 
-	
+
 	@Override
 	public String toString() {
 		return "Student [id=" + id + ", forename=" + forename + ", surname=" + surname + ", dateOfBirth=" + dateOfBirth
